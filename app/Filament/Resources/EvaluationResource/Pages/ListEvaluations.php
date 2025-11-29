3a8c9e5964d5ed27e526759ae86d842b541ece2e
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2025-11-29 16:31:02 +0000

    refactor: Remove unused PDF export functionality from ListEvaluations

--- app/Filament/Resources/EvaluationResource/Pages/ListEvaluations.php
+++ app/Filament/Resources/EvaluationResource/Pages/ListEvaluations.php
@@ -4,11 +4,9 @@ namespace App\Filament\Resources\EvaluationResource\Pages;
 
 use App\Filament\Resources\EvaluationResource;
 use App\Exports\EvaluationReponsesExport;
-use App\Models\Evaluation;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Pages\Actions;
 use Maatwebsite\Excel\Facades\Excel;
-use Barryvdh\DomPDF\Facade\Pdf;
 
 class ListEvaluations extends ListRecords
 {
@@ -17,13 +15,6 @@ class ListEvaluations extends ListRecords
     protected function getActions(): array
     {
         return [
-            Actions\Action::make('exportPdf')
-                ->label('Exporter en PDF')
-                ->icon('heroicon-o-document-download')
-                ->color('danger')
-                ->action(function () {
-                    return $this->exportToPdf();
-                }),
             Actions\Action::make('exportExcel')
                 ->label('Exporter en Excel')
                 ->icon('heroicon-o-document-download')
@@ -32,22 +23,6 @@ class ListEvaluations extends ListRecords
                     return $this->exportToExcel();
                 }),
         ];
-    }
-
-    public function exportToPdf()
-    {
-        $evaluations = $this->getFilteredTableQuery()
-            ->with(['utilisateur', 'reponsesDetails.questionEvaluation'])
-            ->get();
-
-        $pdf = Pdf::loadView('exports.evaluation-reponses-pdf', [
-            'evaluations' => $evaluations,
-        ]);
-
-        return response()->streamDownload(
-            fn () => print($pdf->output()),
-            'evaluations-reponses-' . now()->format('Y-m-d') . '.pdf'
-        );
     }
 
     public function exportToExcel()
